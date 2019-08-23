3ceb1cb2141a0db295e3e21f25bacb15be0c87fb
Thomas Craig <thomas.craig@vac-acc.gc.ca>
2019-08-23 09:05:51 -0300

    updated sql dump file path

--- .circleci/RoboFile.php
+++ .circleci/RoboFile.php
@@ -103,7 +103,7 @@ class RoboFile extends \Robo\Tasks
         $tasks[] = $this->taskFilesystemStack()
             ->copy('.circleci/config/settings.local.php', 'web/sites/default/settings.local.php', $force);
         $tasks[] = $this->taskExec('wget -O dump.sql ' . getenv('DB_DUMP_URL'));
-        $tasks[] = $this->drush()->rawArg('sql:cli < dump.sql -v');
+        $tasks[] = $this->drush()->rawArg('sql:cli < /var/www/html/dump.sql -v');
         return $tasks;
     }
 
